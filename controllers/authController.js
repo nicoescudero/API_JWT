4265e15c0c3cc1d6a6fc66769247320d59288829
nicolas <nicolas.escudero00@gmail.com>
2021-07-18 08:12:55 -0300

    adding functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const verifyToken = require('./verifyToken');
+require('dotenv').config();
 
-router.get('/', async (req, res, next) => {
-    const usuario = await User.find();
-    console.log(usuario);
-    res.json({ message: 'get success' })
+router.get('/me', verifyToken, async (req, res, next) => {
+    const user = await User.findById(req.userId);
+    if (!user)
+        return res.status(404).send('No user found');
+    res.json(user);
 });
+
 router.post('/signup', async (req, res, next) => {
     const { email, password } = req.body;
     const user = new User({
@@ -16,11 +21,29 @@ router.post('/signup', async (req, res, next) => {
 
     user.password = await user.encryptPassword(user.password);
     await user.save();
+    console.log(process.env.KEY)
     console.log(user);
-    res.json({ message: 'User saved success' });
+    const token = jwt.sign({ id: user._id }, process.env.KEY, {//dato que se guarda en el token "id"
+        expiresIn: 60 * 60 * 24//expiracion del token "un dia"
+    });
+    res.json({ auth: true, token });
 });
-router.post('/signin', (req, res, next) => {
-    res.send('This is signin');
+
+router.post('/signin', async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user)
+        return res.status(401).send("The email entered is incorrect or does not exist")
+    const validPassword = await user.validatePassword(password);
+    if (!validPassword)
+        return res.status(401).json({ auth: false, token: null });
+
+    const token = await jwt.sign({ id: user._id }, process.env.KEY, {
+        expiresIn: 60 * 60 * 24
+    })
+    res.json({ auth: true, token });
+
+
 });
 
 module.exports = router;
